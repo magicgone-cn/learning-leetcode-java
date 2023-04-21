e1d8eba0c3cdcb17c9ba6051256b4991c4c42158
华资-杜鹏飞 <dupengfei@sinobest.cn>
2023-04-21 10:27:58 +0800

    Problem15
    使用Stack记录同一个num的数量和当前可用数量

--- src/main/java/cn/magicgone/learning/leetcode/problem15/Solution.java
+++ src/main/java/cn/magicgone/learning/leetcode/problem15/Solution.java
@@ -4,38 +4,34 @@ import java.util.*;
 
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Stack<Integer>> map = new TreeMap<>();
         for (int num : nums) {
-            map.putIfAbsent(num, 0);
-            map.compute(num, (key, oldValue) -> oldValue + 1);
+            map.putIfAbsent(num, new Stack<>());
+            map.get(num).push(num);
         }
 
         List<List<Integer>> result = new ArrayList<>();
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int firstNum = entry.getKey();
-            entry.setValue(entry.getValue() - 1);
-            for (Map.Entry<Integer, Integer> secondEntry : map.entrySet()) {
-                int secondNum = secondEntry.getKey();
-                if (secondEntry.getValue() <= 0 || secondNum < firstNum) {
-                    continue;
-                }
-                int thirdNum = 0 - firstNum - secondNum;
-                if (thirdNum < secondNum) {
-                    continue;
-                }
+        List<Integer> list = new ArrayList<>(map.keySet());
 
-                int thirdNumCount = map.getOrDefault(thirdNum, 0);
-                if (thirdNum == secondNum) {
-                    if (thirdNumCount >= 2) {
+        for (int i = 0; i < list.size(); i++) {
+            Integer firstNum = list.get(i);
+            map.get(firstNum).pop();
+            for (int j = i; j < list.size(); j++) {
+                Integer secondNum = list.get(j);
+                if (map.get(secondNum).isEmpty()) {
+                    continue;
+                }
+                map.get(secondNum).pop();
+
+                Integer thirdNum = 0 - firstNum - secondNum;
+                if (thirdNum >= secondNum) {
+                    if (map.containsKey(thirdNum) && !map.get(thirdNum).isEmpty()) {
                         result.add(Arrays.asList(firstNum, secondNum, thirdNum));
                     }
-                    continue;
                 }
 
-                if (thirdNumCount > 0) {
-                    result.add(Arrays.asList(firstNum, secondNum, thirdNum));
-                }
+                map.get(secondNum).push(secondNum);
             }
         }
 
